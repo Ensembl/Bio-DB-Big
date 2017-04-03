f3fb828466b2bab45865c24c918c10b57234e7db
Andrew Yates <ayates@ebi.ac.uk>
2017-04-03 17:27:39 +0100

    Removed TODO as this has been done.
    We have decided not to support extra calls for non-standard Bed formats
    and documented the reasons why in our Architecture Decision Record.

--- lib/Bio/DB/Big/AutoSQL.pm
+++ lib/Bio/DB/Big/AutoSQL.pm
@@ -18,8 +18,6 @@ limitations under the License.
 
 package Bio::DB::Big::AutoSQL;
 
-#TODO consider bringing across functions to help decipher bigbed files when no AutoSQL has been used to create it or unofficial names have been used
-
 =pod
 
 =head1 NAME
